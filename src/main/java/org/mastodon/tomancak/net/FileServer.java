6997767e9fe89f6d4b74f683e9dd417d0f39fedc
Vladimír Ulman <ulman@fi.muni.cz>
2020-04-07 23:02:51 +0200

    ADD: POST transfers

--- src/main/java/org/mastodon/tomancak/net/FileServer.java
+++ src/main/java/org/mastodon/tomancak/net/FileServer.java
@@ -1,6 +1,7 @@
 package org.mastodon.tomancak.net;
 
-import java.io.InputStream;
+import java.io.*;
+import java.net.URLDecoder;
 import java.nio.file.Paths;
 import java.util.Deque;
 import java.util.Map;
@@ -14,9 +15,9 @@ import io.undertow.server.handlers.resource.PathResourceManager;
 public class FileServer
 {
 	static final int port = 7070;
-
-	static final String filesRootFolder = "/temp";
 	static final long transferMinSize = 100;
+
+	static final String filesRootFolder = "/temp/put";
 
 	public static void main(final String[] args)
 	{
@@ -51,31 +52,42 @@ public class FileServer
 					exchange.dispatch(this);
 					return;
 				}
+				//if we got here, we're in a "worker thread" (and may potentially block ourselves later)
 
 				final Map<String,Deque<String>> params = exchange.getQueryParameters();
 				final Deque<String> _nameValue = params.get("name");
 				final Deque<String> _noOfSpots = params.get("spots");
 				final Deque<String> _noOfLinks = params.get("links");
 
-				final String nameValue = _nameValue != null ?  _nameValue.getFirst() : "fake file";
-				final String noOfSpots = _noOfSpots != null ?  _noOfSpots.getFirst() : "fake spot";
-				final String noOfLinks = _noOfLinks != null ?  _noOfLinks.getFirst() : "fake link";
+				final String nameValue = _nameValue != null ? URLDecoder.decode(_nameValue.getFirst(),"UTF-8") : "fake file";
+				final String noOfSpots = _noOfSpots != null ? _noOfSpots.getFirst() : "fake spot";
+				final String noOfLinks = _noOfLinks != null ? _noOfLinks.getFirst() : "fake link";
 
+				/*
 				System.out.println("Query: name=" + nameValue);
 				System.out.println("Query: spots=" + noOfSpots);
 				System.out.println("Query: links=" + noOfLinks);
+				*/
 
-				System.out.println("req method=" + exchange.getRequestMethod());
-				final InputStream is = exchange.getInputStream();
-				if (is != null)
+				//sanity test...
+				if (!exchange.getRequestMethod().equalToString("POST"))
 				{
-					System.out.println("some input stream:");
-					//System.out.println("post="+ is);
+					System.out.println("Going to complain...");
+					exchange.getResponseSender().send("You gave me no file data!");
 				}
-				else
-				{
-					System.out.println("no input stream");
-				}
+
+				OutputStream fos
+				  = new BufferedOutputStream(
+				    new FileOutputStream(filesRootFolder+File.separator+nameValue) );
+
+				exchange.startBlocking();
+				exchange.getRequestReceiver().receivePartialBytes( (ex,bytes,flag) -> {
+					//System.out.println("found: "+bytes.length);
+					//System.out.println("flag : "+flag);
+					try { if (bytes.length > 0) fos.write(bytes); } catch (IOException e) {/*yes,empty*/}
+				} );
+
+				fos.close();
 			}
 		};
 	}
@@ -83,6 +95,6 @@ public class FileServer
 	public static
 	String fileUploadQueryStringCreate(final String name, final int spotsCnt, final int linksCnt)
 	{
-		return String.format("?name=%s&spots=%u&links=%u",name,spotsCnt,linksCnt);
+		return String.format("?name=%s&spots=%d&links=%d",name,spotsCnt,linksCnt);
 	}
 }
--- src/main/java/org/mastodon/tomancak/net/FileTransfer.java
+++ src/main/java/org/mastodon/tomancak/net/FileTransfer.java
@@ -1,7 +1,7 @@
 package org.mastodon.tomancak.net;
 
-import java.net.URL;
-import java.net.MalformedURLException;
+import java.io.OutputStream;
+import java.net.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.File;
@@ -12,15 +12,15 @@ public class FileTransfer
 {
 	static final String remoteURL = "http://localhost:"+FileServer.port;
 
+	static final String filesRootFolder = "/temp/get";
+
 	static public
 	void getParticularFile(final String filename)
 	{
-		final String localRootFolder = "/temp/get";
-
 		try {
 			Files.copy(
-				new URL(remoteURL + "/" + filename).openStream(),
-				Paths.get(localRootFolder + File.separator + filename)
+				new URL(remoteURL + "/files/" + filename).openStream(),
+				Paths.get(filesRootFolder + File.separator + filename)
 			);
 		} catch (FileNotFoundException e) {
 			System.err.println("Server cannot serve this request:");
@@ -34,8 +34,37 @@ public class FileTransfer
 		}
 	}
 
+	static public
+	void postParticularFile(final String filename, final int spotsCnt, final int linksCnt)
+	{
+		try {
+			final URL url = new URL(remoteURL
+			  + "/put"
+			  + FileServer.fileUploadQueryStringCreate(URLEncoder.encode(filename,"UTF-8"),spotsCnt,linksCnt));
+			//System.out.println("talking to: "+url);
+
+			final HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			Files.copy(
+				Paths.get(filesRootFolder + File.separator + filename),
+				conn.getOutputStream()
+			);
+
+			//this actually makes the whole thing spinning (does not send out a byte without this command)
+			conn.getInputStream();
+		} catch (MalformedURLException e) {
+			System.err.println("Cannot connect to the server:");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("Cannot write the output file:");
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args)
 	{
-		getParticularFile("displaySettings.txt");
+		//getParticularFile("displaySettings.txt");
+		postParticularFile("FR.txt",10,20);
 	}
 }
