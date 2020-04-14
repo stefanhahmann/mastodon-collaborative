1b52004c69b8843046fdcc08b2c6635e3c23c340
Vladimír Ulman <ulman@fi.muni.cz>
2020-04-14 23:39:46 +0200

    ADD: merge params harvesting dialog created

--- src/test/java/org/mastodon/tomancak/clearPrefs.java
+++ src/test/java/org/mastodon/tomancak/clearPrefs.java
@@ -1,6 +1,7 @@
 package org.mastodon.tomancak;
 
 import net.imagej.ImageJ;
+import org.mastodon.tomancak.util.MergeModelDialog;
 
 public class clearPrefs
 {
@@ -9,6 +10,7 @@ public class clearPrefs
 		final ImageJ IJ = new ImageJ();
 		IJ.prefs().clear(LoadEarlierProgress.class);
 		IJ.prefs().clear(ReportProgress.class);
+		IJ.prefs().clear(MergeModelDialog.class);
 		System.out.println("RACING plugins' preferences cleared");
 	}
 }
