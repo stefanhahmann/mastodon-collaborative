6e06f7e5b5bb8c40c10d11f81f9a76742ddc6be0
Vladimír Ulman <ulman@fi.muni.cz>
2020-05-14 18:54:18 +0200

    ADD: Menu item to start solo SciView

---
+++ src/main/java/org/mastodon/tomancak/SciViewPlugin.java
@@ -0,0 +1,121 @@
+package org.mastodon.tomancak;
+
+import org.mastodon.app.ui.ViewMenuBuilder;
+import org.mastodon.plugin.MastodonPlugin;
+import org.mastodon.plugin.MastodonPluginAppModel;
+import org.mastodon.revised.mamut.KeyConfigContexts;
+import org.mastodon.revised.mamut.MamutAppModel;
+import org.mastodon.revised.ui.keymap.CommandDescriptionProvider;
+import org.mastodon.revised.ui.keymap.CommandDescriptions;
+
+import org.scijava.AbstractContextual;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.behaviour.util.AbstractNamedAction;
+import org.scijava.ui.behaviour.util.Actions;
+import org.scijava.ui.behaviour.util.RunnableAction;
+
+import bdv.viewer.SourceAndConverter;
+import sc.iview.SciView;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
+
+import static org.mastodon.app.ui.ViewMenuBuilder.item;
+import static org.mastodon.app.ui.ViewMenuBuilder.menu;
+
+@Plugin( type = MastodonPlugin.class )
+public class SciViewPlugin extends AbstractContextual implements MastodonPlugin
+{
+	private static final String SCIVIEW_CONNECTOR = "[tomancak] start SciView";
+	private static final String[] SCIVIEW_CONNECTOR_KEYS = { "not mapped" };
+
+	private static Map< String, String > menuTexts = new HashMap<>();
+	static
+	{
+		menuTexts.put( SCIVIEW_CONNECTOR, "Start SciView" );
+	}
+
+	@Override
+	public Map< String, String > getMenuTexts()
+	{
+		return menuTexts;
+	}
+
+	@Override
+	public List< ViewMenuBuilder.MenuItem > getMenuItems()
+	{
+		return Arrays.asList(
+				menu( "Plugins",
+						item( SCIVIEW_CONNECTOR ) ) );
+	}
+
+	/** Command descriptions for all provided commands */
+	@Plugin( type = CommandDescriptionProvider.class )
+	public static class Descriptions extends CommandDescriptionProvider
+	{
+		public Descriptions()
+		{
+			super( KeyConfigContexts.TRACKSCHEME, KeyConfigContexts.BIGDATAVIEWER );
+		}
+
+		@Override
+		public void getCommandDescriptions( final CommandDescriptions descriptions )
+		{
+			descriptions.add( SCIVIEW_CONNECTOR, SCIVIEW_CONNECTOR_KEYS, "Start SciView and a special Mastodon->SciView connector control panel." );
+		}
+	}
+
+
+	private final AbstractNamedAction startSciViewConnectorAction;
+
+	public SciViewPlugin()
+	{
+		startSciViewConnectorAction = new RunnableAction( SCIVIEW_CONNECTOR, this::startSciViewConnector );
+		updateEnabledActions();
+	}
+
+	private MastodonPluginAppModel pluginAppModel;
+
+	@Override
+	public void setAppModel( final MastodonPluginAppModel model )
+	{
+		this.pluginAppModel = model;
+		updateEnabledActions();
+	}
+
+	@Override
+	public void installGlobalActions( final Actions actions )
+	{
+		actions.namedAction( startSciViewConnectorAction, SCIVIEW_CONNECTOR_KEYS );
+	}
+
+	private void updateEnabledActions()
+	{
+		final MamutAppModel appModel = ( pluginAppModel == null ) ? null : pluginAppModel.getAppModel();
+		startSciViewConnectorAction.setEnabled( appModel != null );
+	}
+
+
+	private void startSciViewConnector()
+	{
+		new Thread("Mastodon's SciView")
+		{
+			@Override
+			public void run()
+			{
+				SciView sv;
+				try {
+					sv = SciView.create();
+					Thread.sleep(2000); //a bit of a grace time before we continue
+					System.out.println("SciView started...");
+				} catch (Exception e) {
+					System.out.println("SciView has a problem:");
+					e.printStackTrace();
+					return;
+				}
+			}
+		}.start();
+	}
+}
