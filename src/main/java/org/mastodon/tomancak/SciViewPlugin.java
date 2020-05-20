46258163af99180c8c0de41766f6de2418eb98b9
Vladimír Ulman <ulman@fi.muni.cz>
2020-05-21 00:45:20 +0200

    COS: code cleaned up, should have the same functionality

--- src/main/java/org/mastodon/tomancak/SciViewPlugin.java
+++ src/main/java/org/mastodon/tomancak/SciViewPlugin.java
@@ -1,15 +1,13 @@
 package org.mastodon.tomancak;
 
-import bdv.tools.brightness.ConverterSetup;
 import bdv.util.Bounds;
-import bdv.viewer.Source;
+import bdv.viewer.ConverterSetups;
 import graphics.scenery.Sphere;
 import graphics.scenery.volumes.Colormap;
 import graphics.scenery.volumes.TransferFunction;
 import graphics.scenery.volumes.Volume;
 
 import net.imglib2.display.ColorTable8;
-import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.type.numeric.ARGBType;
 import org.joml.*;
 import org.mastodon.app.ui.ViewMenuBuilder;
@@ -33,8 +31,6 @@ import bdv.viewer.SourceAndConverter;
 import sc.iview.SciView;
 import sc.iview.commands.view.SetTransferFunction;
 
-import java.lang.Math;
-import java.text.NumberFormat;
 import java.util.List;
 import java.util.Arrays;
 import java.util.Map;
@@ -136,27 +132,8 @@ public class SciViewPlugin extends AbstractContextual implements MastodonPlugin
 
 				final String volumeName = "Mastodon's raw data";
 				final SourceAndConverter<?> sac = pluginAppModel.getAppModel().getSharedBdvData().getSources().get(0);
-
-				final Source<?> s = sac.getSpimSource();
-				final float[] voxelDims = new float[s.getVoxelDimensions().numDimensions()];
-				for (int d = 0; d < voxelDims.length; ++d)
-				{
-					voxelDims[d] = (float)s.getVoxelDimensions().dimension(d);
-					//System.out.print(voxelDims[d]+"  ");
-				}
-				//System.out.println("are the voxel dimensions");
-
-				//image size -- Ulrik suggested that spot coords should be normalized w.r.t. image size,
-				//but it turned out this is not needed.. so this piece of code has no use
-				final long imgSize[] = new long[3];
-				s.getSource(0,0).dimensions(imgSize);
-				System.out.println("ImgSize: "+printArray(imgSize));
-
-				//crank up the volume :-)
+				//
 				final Volume v = (Volume)sv.addVolume((SourceAndConverter)sac, volumeName);
-
-				setVolumeColorFromMastodon(v);
-				//may setup own display range if need-be
 
 				//adjust the transfer function to a "diagonal"
 				TransferFunction tf = v.getTransferFunction();
@@ -166,31 +143,30 @@ public class SciViewPlugin extends AbstractContextual implements MastodonPlugin
 				tf.addControlPoint(0.90f, 0.7f);
 				tf.addControlPoint(1.00f, 0.8f);
 
+				setVolumeColorFromMastodon(v);
+				//may setup own display range if need-be
+
+				v.setName(volumeName);
+				v.setWantsComposeModel(false); //makes position,scale,rotation be ignored, also pxToWrld scale is ignored
+				final float scale = 0.5f;      //alternative to v.setPixelToWorldRatio(scale);
+				v.setModel( new Matrix4f(scale,0,0,0, 0,-scale,0,0, 0,0,-scale,0, 0,0,0,1) );
+				v.setNeedsUpdateWorld(true);
+				//now the volume's diagonal in world coords is now:
+				//      [0,0,0] -> [scale*origXSize, -scale*origYSize, -scale*origZSize]
+
+				//define various listeners:
+				//
+				//watch when BDV (through its color&brightness dialog) changes display range or volume's color
 				pluginAppModel.getAppModel().getSharedBdvData().getConverterSetups().listeners().add( t -> {
 					System.out.println("BDV says display range: " + t.getDisplayRangeMin() + " -> " + t.getDisplayRangeMax());
 					System.out.println("BDV says new color    : " + t.getColor());
+
+					//request that the volume be repainted in SciView
 					setVolumeColorFromMastodon(v);
-
-					//does not work (even with v.setDirty() and company)
-					//v.getConverterSetups().get(0).setDisplayRange(t.getDisplayRangeMin(),t.getDisplayRangeMax());
-
-					//does not work (even with v.setDirty() and company)
-					//final ConverterSetup cs = pluginAppModel.getAppModel().getSharedBdvData().getConverterSetups().getConverterSetup(sac);
-					//pluginAppModel.getAppModel().getSharedBdvData().getConverterSetups().getBounds().setBounds(cs, new Bounds(t.getDisplayRangeMin(),t.getDisplayRangeMax()));
-
-					//does not work (even with v.setDirty() and company)
-					//need to notify of the "repaint request" explicitly because this change is not triggered from
-					//SciView itself and hence it does not know about (and does not "repaint" automatically)
 					v.setDirty(true);
-					v.setNeedsUpdate(true);
-					v.setNeedsUpdateWorld(true);
-					/*
-					*/
-
-					//plan D?
 				});
 
-				//optionally: watch when SciView's control panel adjusts the color
+				//watch when SciView's control panel adjusts the color
 				//and re-reset it back to that of Mastodon
 				v.getConverterSetups().get(0).setupChangeListeners().add( (t) -> {
 					//read out the current min-max setting (which has been just re-set via the SciView's nodes panel)
@@ -204,35 +180,11 @@ public class SciViewPlugin extends AbstractContextual implements MastodonPlugin
 					//ignores (by re-setting back) whatever LUT choice has been made in SciView's nodel panel
 					setVolumeColorFromMastodon(v);
 
-					/* didn't see this working
-					pluginAppModel.getAppModel().getSharedBdvData().getConverterSetups().getConverterSetup(
-						pluginAppModel.getAppModel().getSharedBdvData().getSources().get(0))
-							.setDisplayRange(min,max);
-					*/
-
-					final ConverterSetup cs = pluginAppModel.getAppModel().getSharedBdvData().getConverterSetups().getConverterSetup(sac);
-					pluginAppModel.getAppModel().getSharedBdvData().getConverterSetups().getBounds().setBounds(cs, new Bounds(min,max));
+					final ConverterSetups setups = pluginAppModel.getAppModel().getSharedBdvData().getConverterSetups();
+					setups.getBounds().setBounds( setups.getConverterSetup(sac), new Bounds(min,max) );
 					pluginAppModel.getWindowManager().forEachBdvView( view -> view.requestRepaint() );
 				});
 
-				try {
-					Thread.sleep(3000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-
-				v.setName(volumeName);
-				v.setWantsComposeModel(false); //makes position,scale,rotation be ignored, also pxToWrld scale is ignored
-				final float scale = 0.5f;      //alternative to v.setPixelToWorldRatio(scale);
-				v.setModel( new Matrix4f(scale,0,0,0, 0,-scale,0,0, 0,0,-scale,0, 0,0,0,1) );
-				v.setNeedsUpdateWorld(true);
-				//now the volume's diagonal in world coords is now:
-				//      [0,0,0] -> [scale*origXSize, -scale*origYSize, -scale*origZSize]
-
-				System.out.println("VOlume is from "
-					+ v.getBoundingBox().getMin().toString(NumberFormat.getNumberInstance())
-					+ " to "
-					+ v.getBoundingBox().getMax().toString(NumberFormat.getNumberInstance()) );
 
 				//start the TransferFunction modifying dialog
 				getContext().getService(CommandService.class).run(SetTransferFunction.class,true,
@@ -254,19 +206,10 @@ public class SciViewPlugin extends AbstractContextual implements MastodonPlugin
 					sph.setParent(v);
 				}
 
-				float[] matrixData = new float[16];
-				v.getModel().get(matrixData);
-				System.out.println("Volume model matrix: "+printArray(matrixData));
-				v.getWorld().get(matrixData);
-				System.out.println("Volume world matrix: "+printArray(matrixData));
-				v.getModelView().get(matrixData);
-				System.out.println("Volume modelView matrix: "+printArray(matrixData));
-
 				//TODO: does not update the scene graph panel, consider using:
 				//private EventService eventService;
 				//eventService.publish(new NodeAddedEvent(n));
 			}
-
 		}.start();
 	}
 
@@ -319,27 +262,6 @@ public class SciViewPlugin extends AbstractContextual implements MastodonPlugin
 			map[1][i] = (byte)(ratio * (float)g);
 			map[2][i] = (byte)(ratio * (float)b);
 		}
-		/* DEBUG:
-		System.out.println("map[0] = "  +map[0][0]+","+map[1][0]+","+map[2][0]);
-		System.out.println("map[80] = " +map[0][80]+","+map[1][80]+","+map[2][80]);
-		System.out.println("map[160] = "+map[0][160]+","+map[1][160]+","+map[2][160]);
-		System.out.println("map[255] = "+map[0][255]+","+map[1][255]+","+map[2][255]);
-		*/
 		return map;
-	}
-
-	void reportTransform(final Source<?> s)
-	{
-		final AffineTransform3D t = new AffineTransform3D();
-		s.getSourceTransform(0,0,t);
-
-		for (double d : t.getRowPackedCopy()) System.out.print(d+",");
-		System.out.println();
-	}
-
-
-	public static void main(String[] args) {
-		long[] data = new long[] {3, 5, 6};
-		System.out.println("data: "+printArray(data));
 	}
 }
